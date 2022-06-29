85ffb4dfa59cf8ecf6c770f105fa45e1b0a46fce
Raj <aakash.raj@siemens.com>
2022-06-29 13:35:56 +0530

    Initial Commit

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,27 +1,36 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-import './Navbar.css'
+import "./Navbar.css";
 
 export default function Navbar() {
-
-    return (
-        <div className='home-container'>
-            <div className='home-header'>
-                <h1 className='home-heading'>Yoga Studio</h1>
-                <Link to='/about'>
-                    <button 
-                        className="btn btn-secondary" 
-                        id="about-btn"
-                    >
-                        About
-                    </button>
-                </Link>
-            </div>
-
-            
-
-        </div>
-            
-    )
+  return (
+    <div className="home-container" style={{ height: "auto" }}>
+      <div className="home-header">
+        <Link to={"/"} style={{ cursor: "pointer", textDecoration: "none" }}>
+          <h1 className="home-heading">Yoga Studio</h1>
+        </Link>
+        <Link to="/about">
+          <button
+            className=""
+            style={{
+              background: "violet",
+              border: "none",
+              color: "black",
+              //   fontWeight: "100",
+              paddingRight: "1rem",
+              paddingLeft: "1rem",
+              paddingTop: "0.5rem",
+              paddingBottom: "0.5rem",
+              border: "none",
+              borderRadius: "9px",
+              marginRight: "2rem",
+            }}
+          >
+            About
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 }
