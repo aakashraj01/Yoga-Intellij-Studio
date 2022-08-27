dc5321396eb05aefd0e46a28fa9def014f04f84d
Pranshu Srivastava <iampranshu24@gmail.com>
2022-08-27 14:46:33 +0530

    Improving the About page.

--- frontend/src/pages/About/About.js
+++ frontend/src/pages/About/About.js
@@ -1,33 +1,34 @@
-import React from 'react'
+import React from "react";
 
-import './About.css'
+import "./About.css";
 
 export default function About() {
-    return (
-        <div className="about-container">
-            <h1 className="about-heading">About</h1>
-            <div className="about-main">
-                <p className="about-content">
-                    This is an realtime AI based Yoga Trainer which detects your pose how well you are doing.
-                    We deployed this project so people can use it to o yoga with correct asanas.
-                    This is an open source project, The code is available on the GitHub
-                    
-                    This AI first predicts keypoints or coordinates of different parts of the body(basically where
-                    they are present in an image) and then it use another classification model to classify the poses if 
-                    someone is doing a pose and if AI detects that pose more than 95% probability and then it will notify you are 
-                    doing correctly(by making virtual skeleton green). We have used Tensorflow pretrained Movenet Model To Predict the 
-                    Keypoints and building a neural network top of that which uses these coordinates and classify a yoga pose.
-
-                    We have trained the model in python because of tensorflowJS we can leverage the support of browser so we converted 
-                    the keras/tensorflow model to tensorflowJS.
-                </p>
-                <div className="developer-info">
-                    <h4>About Developers</h4>
-                    <p className="about-content">We are team DeSync 
-                    </p>
-                    
-                </div>
-            </div>
+  return (
+    <div className="about-container">
+      <h1 className="about-heading">About</h1>
+      <div className="about-main">
+        <p className="about-content">
+          This is a realtime AI based Yoga Trainer which detects your posture
+          and lets you know if you are doing an aasan properly. We have deployed
+          this project and made it open source so that more people can use it
+          and contribute more aasanas to it. This AI first identifies the
+          keypoints or the coordinates of different parts of the person's
+          body(basically where they are present in an image) and using it, it
+          identifies the person's pose and uses a classification model to tell
+          if pose matches one of the aasanas in the repository or not. If the
+          pose resembles an actual aasan with 95% accuracy, then the indicators
+          on the screen turn green indicating that the pose is correct.The model
+          that we have used is a pretrained Tensorflow Movenet Model to predict
+          the keypoints and build a neural network top of that which uses these
+          coordinates and classify the yoga pose. We have trained the model in
+          python because of tensorflowJS we can leverage the support of browser
+          so we converted the keras/tensorflow model to tensorflowJS.
+        </p>
+        <div className="developer-info">
+          <h4>About Developers</h4>
+          <p className="about-content">We are team DeSync</p>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
